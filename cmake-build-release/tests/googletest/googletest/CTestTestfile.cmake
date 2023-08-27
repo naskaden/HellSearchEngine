d36c4548ccfe75448d0c2853b4305669287bc33d
Anastasiia <nastya_30.1996@mail.ru>
2023-08-27 18:54:49 +0300

    Update

---
+++ cmake-build-release/tests/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/GitHub/HellSearchEngine/tests/googletest/googletest
+# Build directory: D:/GitHub/HellSearchEngine/cmake-build-release/tests/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
